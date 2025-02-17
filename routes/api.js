a6be0d0cb1bc77ea71bfe4f4a458eadf764cc7c8
YvanMoris <yvan.medano@gmail.com>
2025-02-17 19:23:48 +0100

    api

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,42 @@
 'use strict';
-
+const axios = require('axios');
 module.exports = function (app) {
+// 📌 Función para obtener el precio de una acción
+  function getStockPrice  (symbol) {
+    const url = 'https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote';
+    return axios.get(url)
+    .then(response => response.data.latestPrice)
+    .catch( () => null); // Si la acción no existe
 
+  };
   app.route('/api/stock-prices')
     .get(function (req, res){
+      let { stock, like } = req.query;
+      let stocks = Array.isArray(stock) ? stock : [stock];
+      const userIP = anonymizeIP(req.ip);
+
+      //Construimos res.json en función del request:
+      //  stock, stock+true, stock+stock, stock+stock+true
+      let stockData = [];
+      for(let i = 0; i< stocks.length; i++){
+        let symbol = stocks[i];
+        let price = getStockPrice(symbol);
+        stockData.push({
+          "stock" : symbol.toUpperCase(),
+          "price" : price
+        });
+      }
       
+      if (stocks.length === 1) {
+        res.json({ stockData: stockData[0] });
+      } else if (stocks.length === 2) {
+        res.json({
+          stockData: [
+            stockData[0],
+            stockData[1]
+          ]
+        });
+      }
     });
     
 };
